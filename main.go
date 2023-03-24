2b4ce40785b900036184b02b785343a4ec5bb2c8
Aryansh <500093038@stu.upes.ac.in>
2023-03-24 14:16:23 +0530

    Validating user input for optimizing

--- main.go
+++ main.go
@@ -54,30 +54,45 @@ func main() {
 		fmt.Println("enter number of tickets: ")
 		fmt.Scan(&userTicket)
 
-		if userTicket > RemainingTickets {
-			fmt.Printf("Sorry we have only %v tickets available, you can not book %v tickets so book only in range\n", RemainingTickets, userTicket)
-			continue
+		//checking user validation
+
+		isvalidName := len(firstName) >= 2 && len(lastName) >= 2
+		isvalidEmail := strings.Contains(email, "@")
+		isvalidTickets := userTicket > 0 && userTicket <= RemainingTickets
+
+		if isvalidName && isvalidEmail && isvalidTickets {
+			RemainingTickets = RemainingTickets - userTicket
+			//bookings[0] = firstName + " " + lastName
+			bookings = append(bookings, ","+firstName+" "+lastName)
+
+			fmt.Printf("Thank you %v %v for booking %v tickets. You will recieve notifications regarding conference on you email %v\n", firstName, lastName, userTicket, email)
+			fmt.Printf("now %v tickets remaining for %v\n", RemainingTickets, ConferenceName)
+
+			//printing length of array
+			//fmt.Printf("Length of slice- %v\n", len(bookings))
+			firstnames := []string{}
+			for _, booking := range bookings {
+				// it expect two values from bookings , so we have to use two variables to coolece thos values but we are not using first value that is index , so instead of using index we can use underscore
+				// _ are used to represent those values which we don't want to use
+				var names = strings.Fields(booking)
+
+				firstnames = append(firstnames, names[0])
+			}
+
+			fmt.Printf("The firstnames of all the persons who booked a ticket %v\n", firstnames)
+		} else {
+			if !isvalidName {
+				fmt.Println("User input name is invalid,They are very short")
+			}
+			if !isvalidEmail {
+				fmt.Println("User input email is invalid, It does not have @")
+			}
+			if !isvalidTickets {
+				fmt.Println("User Input tickets are invalid")
+			}
+			fmt.Println("Try again!")
+			//fmt.Println("Your input Data is invalid, Try again!")
 		}
-
-		RemainingTickets = RemainingTickets - userTicket
-		//bookings[0] = firstName + " " + lastName
-		bookings = append(bookings, ","+firstName+" "+lastName)
-
-		fmt.Printf("Thank you %v %v for booking %v tickets. You will recieve notifications regarding conference on you email %v\n", firstName, lastName, userTicket, email)
-		fmt.Printf("now %v tickets remaining for %v\n", RemainingTickets, ConferenceName)
-
-		//printing length of array
-		//fmt.Printf("Length of slice- %v\n", len(bookings))
-		firstnames := []string{}
-		for _, booking := range bookings {
-			// it expect two values from bookings , so we have to use two variables to coolece thos values but we are not using first value that is index , so instead of using index we can use underscore
-			// _ are used to represent those values which we don't want to use
-			var names = strings.Fields(booking)
-
-			firstnames = append(firstnames, names[0])
-		}
-
-		fmt.Printf("The firstnames of all the persons who booked a ticket %v\n", firstnames)
 
 	}
 
